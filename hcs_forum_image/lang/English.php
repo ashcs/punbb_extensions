9b61401ae83c44b0964dc1f7dd58f3324aa8a11a
ashcs <ashcs@.(none)>
2011-08-14 22:55:00 +0700

    add forum_image extension

---
+++ hcs_forum_image/lang/English.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang_forum_f_image = array(
+'Forum image'		=>	'Forum image',
+'forum head'		=>	'Images Control',
+'forum info'		=>	'Here you can download the image (banner), which will be displayed on the main page with the name and description of the forum in the category',
+'current image'		=>	'Current image',
+'Delete image info'	=>	'Click to remove the current image',
+'No image info'		=>	'No image',
+'Upload image file'	=>	'Upload image',
+'Image upload help'	=>	'The image must be a graphic format png, jpg or gif.',
+'Image place'		=>	'Position',
+'Place left'		=>	'Left',
+'Place right'		=>	'Right',
+'Image link'		=>	'ULR',
+'Image link help'	=>	'Enter the link, which will keep this image, without <strong>http://</strong>',
+);
